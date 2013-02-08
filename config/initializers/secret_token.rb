25048c4a12c6770bd766cbcd5fd396d773a50caf
Michael DeVerna <michaeldeverna@yahoo.com>
2013-02-07 21:16:18 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Graphapp::Application.config.secret_token = 'ba4fb9301d3762689050b4f8759490256af4f29491e2b82bc2bb5f3ec537ef4a64f397aa28b6faecef8075b58cc6fa19ead9634c8abaee669b328c1d57e85a00'
